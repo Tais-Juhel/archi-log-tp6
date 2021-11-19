1797d0a6b0f3c1a03b4b835c0003eb975c58ada2
nairod49 <78696310+nairod49@users.noreply.github.com>
2021-11-19 15:51:02 +0100

    Update capitalize.js

--- filters/capitalize.js
+++ filters/capitalize.js
@@ -1,5 +1,12 @@
 module.exports = (input) => {
+<<<<<<< Updated upstream
     let textMaj=input.toUpperCase();
     console.log(textMaj);
     return textMaj;
+=======
+
+let textMaj=input.toUpperCase();
+
+return textMaj;
+>>>>>>> Stashed changes
 }
